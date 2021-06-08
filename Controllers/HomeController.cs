48dd9d0cf026f7c0c366537583204d855dd4f4bf
Your Name <email@example.com>
2021-06-08 14:10:28 +0000

    finishing touches completed - App is ready for publishing

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,10 +23,17 @@ namespace MvcMovie.Controllers
             return View();
         }
 
+        public IActionResult Movies()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
         }
+
+      
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
